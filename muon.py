Fixes some issues with muon calculations that took vector input. Now have total production working for vectors of input, with scaling.

muon.py
@@ -76,7 +76,7 @@ def p_fast_slhl(z, n):
     z is depth in g/cm2
     n is the a nuclide object such as Be10Qtz
     """
-    return nuc.sigma0 * nuc.Natoms * beta(z) * phi_slhl(z) * ebar(z) ** ALPHA
+    return n.sigma0 * n.Natoms * beta(z) * phi_slhl(z) * ebar(z) ** ALPHA
 
 def p_fast(z, flux, nuc):
     """
@@ -108,7 +108,7 @@ def Rv0(z):
     else:
         f = (121100.0 / z)**2
         g = np.exp(-z / 121100.0)
-        dfdz = -2.0 * (121100)**2
+        dfdz = -2 * (121100)**2
         dgdz = -np.exp(-z/121100.0) / 121100.0
         stop_rate = -1.82e-6 * (dfdz * g + dgdz * f)
     return stop_rate 
@@ -138,20 +138,33 @@ def LZ(z):
 	P_MeVc = np.exp(ifnc(np.log(z)))
 	return 263 + 150 * (P_MeVc / 1000.0)
 
-def P_mu_total(z, h, nuc, is_alt=False):
+def P_mu_total(z, h, nuc, is_alt=True):
     """
     Total production rate from muons
     
     Takes:
     z: a vector of depths
-    h: atmospheric pressure in hPa at surface (or altitude in m, see below)
+    h: altitude in meters or the atmospheric pressure in hPa at surface
     n: a nuclide object containing nuclide specific information
-    is_alt (optional): makes h be treated as 
+    is_alt (optional): makes h be treated as an altitude in meters
     """
     t = type(z)
-    if (t == 'numpy.ndarray' or t == 'list') and len(z) != len(h):
+    is_array = (t == np.ndarray or t == list)
+    if is_array and len(z) != len(h):
         raise ValueError("z and h must be arrays of the same length")
-    
+
+    if is_array:
+        j = 0
+        zmod = np.zeros(len(z))
+        for i, zi in enumerate(z):
+            if zi < 1:
+                zi = 1
+            zmod[j] = zi
+    else:
+        if z < 1:
+            zmod = 1
+
+    # if h is an altitude instead of pressure, convert to pressure
     if is_alt:
          h = scaling.alt_to_p(h)
     
@@ -165,17 +178,17 @@ def P_mu_total(z, h, nuc, is_alt=False):
     R_v0 = Rv0(z)
     
     # calculate vertical muon stopping rate at the site
-    L = LZ(z)
+    L = LZ(zmod)
     R_v = R_v0 * np.exp(H / L)
     
     # integrate the stopping rate to get the vertical muon flux at depth z
     # at the sample site
     z = np.array(z)
-    phi_v = np.zeros(len(z))
+    phi_v = int_err = np.zeros(len(z))
     i = 0
     for zi in z:
-        phi_v_tmp = integrate.quad(lambda x: Rv0(x) * np.exp(H[i]/L[i]), zi, 2e5)
-        phi_v[i] = phi_v_tmp[0]
+        phi_v[i], int_err[i] = \
+            integrate.quad(lambda x: Rv0(x) * np.exp(H[i]/L[i]), zi, 2e5)
         i += 1
     
     # add in the flux below 2e5 g / cm2, assumed to be constant
@@ -187,7 +200,7 @@ def P_mu_total(z, h, nuc, is_alt=False):
     # calculate total muon stopping rate at depth z + H
     # R = derivative(tot_muon_flux(z+H))
     nofz = n(z + h)
-    dndz = -0.297 / ((z + H) + 4200) + 1.21e-5 # derivative of 
+    dndz = -0.297 / ((z + H) + 4200) + 1.21e-5 # derivative of n(z+H)
     R = 2 * pi * (R_v / (nofz + 1) - phi_v * nofz**-2 * dndz)
     
     R_neg = F_NEGMU * R # stopping rate of negative muons
@@ -199,7 +212,3 @@ def P_mu_total(z, h, nuc, is_alt=False):
     
     return {'P_tot': P_tot, 'P_fast': P_fast, 'P_neg': P_neg, 'L': L, 'R': R, \
             'phi': phi, 'H': h, 'phi_v': phi_v, 'R_v': R_v, 'phi_v0': phi_v0, 'R_v0': R_v0}
-    
-    
-    
-    

scaling.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import numpy as np
+import scipy as sp
 from numpy import exp, log, arange, array, abs
 import np_util as util
 import scipy.interpolate
@@ -30,11 +31,11 @@ def stone2000(lat, P=None, Fsp=0.978, alt=None):
     supplied we default to sea level pressure.
     """
     if P == None:
-    	if alt == None:
-        	P = 1013.25
+        if alt == None:
+            P = 1013.25
         else:
-			P = alt_to_p(alt)
-    
+            P = alt_to_p(alt)
+        
     a = array([31.8518,    34.3699,    40.3153,    42.0983,    56.7733,    69.0720,    71.8733])
     b = array([250.3193,   258.4759,   308.9894,   512.6857,   649.1343,   832.4566,   863.1927])
     c = array([-0.083393,  -0.089807,  -0.106248,  -0.120551,  -0.160859,  -0.199252,  -0.207069])
@@ -51,24 +52,36 @@ def stone2000(lat, P=None, Fsp=0.978, alt=None):
         lat = array([x if x < 60 else 60 for x in lat])
     else:
         if lat > 60: lat = 60.0
+    lat = np.array(lat)
     
     # create ratios for 0 through 60 degrees by ten degree intervals
     n = range(len(ilats))
     # calculate scaling factors for index latitudes
     f_lat = [a[x] + b[x] * exp(-P/150.0) + c[x] * P + d[x] * P**2 + e[x] * P**3 for x in n]
-    # interpolate between the index latitud scaling factors and evaluate
+
+    # for requests with multiple latitudes we need to transpose the result
+    f_lat = np.transpose(f_lat)
+    
+    # interpolate between the index latitude scaling factors and evaluate
     # the interpolation function at each sample latitude
-    S = scipy.interpolate.interp1d(ilats, f_lat)(lat)
+    S = np.zeros(len(lat))
+    for i, ifactors in enumerate(f_lat):
+        S[i] = sp.interpolate.interp1d(ilats, ifactors)(lat[i])
     
     # muon scaling
     mk = array([0.587, 0.6, 0.678, 0.833, 0.933, 1.0, 1.0])
-    fm_lat = mk * exp((1013.25 - P) / 242.0)
+    fm_lat = [mk_i * exp((1013.25 - P) / 242.0) for mk_i in mk]
+    fm_lat = np.transpose(fm_lat)
     # get muon scaling factors
-    M = scipy.interpolate.interp1d(ilats, fm_lat)(lat)
+    
+    M = np.zeros(len(lat))
+    for i, ifactors in enumerate(fm_lat):
+        M[i] = sp.interpolate.interp1d(ilats, ifactors)(lat[i])
     
     scalingfactors = S * Fsp + M * (1 - Fsp)
     
     return scalingfactors
+
 """
 Elsasser et al. 1956 via Dunai's book:
 M = dipole moment [m2 / A]