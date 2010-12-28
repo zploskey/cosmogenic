cacf02bfd1101d55d78a2ebdae0d6dd9206a5287
Zach Ploskey <zploskey@uw.edu>
2010-12-28 11:37:01 -0800

    Overexposure histogram code cleaned up

--- litdata_hist.py
+++ litdata_hist.py
@@ -1,11 +1,10 @@
 # litdata_hist
 
-import numpy as np
-import scipy as sp
+from numpy import genfromtxt
 import matplotlib.pyplot as plt
 
 # Get literature data, currently in percent inherited
-data = np.genfromtxt("inherit_litdata.txt")
+data = genfromtxt("inherit_litdata.txt")
 
 plt.hist(data, 14, range=(0,1400), facecolor='0.8')
 plt.xlim(-50, 1450)
