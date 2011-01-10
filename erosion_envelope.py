6251952b464e6e1308a8f9048d65fda069049f96
Zach Ploskey <zploskey@uw.edu>
2011-01-10 13:32:50 -0800

    Made safe to be imported.

--- erosion_envelope.py
+++ erosion_envelope.py
@@ -5,18 +5,6 @@ import matplotlib.pyplot as plt
 import sim
 import nuclide
 
-tsim = 2.0 # Myr
-er = 25.0  # m / Myr
-z0 = er * tsim # initial depth
-n = 20 # number of events
-lat = 65
-h = 1
-rho = 2.67
-
-tHol = 0.014 # Myr postglacial exposure
-
-be10 = nuclide.Be10Qtz()
-
 def get_zoft(tHol):
     times = np.random.rand(2*n) * (tsim - tHol)
     times.sort() # do an in-place sort of the data
@@ -34,41 +22,54 @@ def get_zoft(tHol):
     depths = np.append([0,0], depths)
     return (depths, times)
 
-plt.figure(1)
+if __name__ == '__main__':
+    tsim = 2.0 # Myr
+    er = 25.0  # m / Myr
+    z0 = er * tsim # initial depth
+    n = 20 # number of events
+    lat = 65
+    h = 1
+    rho = 2.67
 
-ncurves = 100
-depths = np.zeros((ncurves, 2*(n+1)))
-times  = np.zeros((ncurves, 2*(n+1)))
-for i in range(ncurves):
-    depths[i], times[i] = get_zoft(tHol)
-    plt.plot(times[i], depths[i], lw=1)
-plt.xlim((-0.1,tsim+0.1))
-plt.ylim((-1,z0+1))
-plt.gca().invert_xaxis()
-plt.gca().invert_yaxis()
+    tHol = 0.014 # Myr postglacial exposure
 
-plt.figure(2)
+    be10 = nuclide.Be10Qtz()
 
-z = np.linspace(0,20*100.0*rho,50)
-ndzs = 0
-conc = np.zeros((ncurves,len(z)))
-for i in range(ncurves):
-    d = depths[i][1:-1]
-    ndzs = len(d) - 1
-    dz = np.zeros(ndzs)
-    for j in range(ndzs):
-        dz[j] = d[j+1] - d[j]
-    dz = dz.copy()[0::2]
-    t = 1e6 * times[i][1:]
-    conc[i] = sim.fwd_profile(z, dz, t, be10, h, lat)
-    plt.semilogx(conc[i], z / 100.0 / rho, lw=1)
+    plt.figure(1)
 
-plt.gca().invert_yaxis()
-plt.show()
+    ncurves = 10
+    depths = np.zeros((ncurves, 2*(n+1)))
+    times  = np.zeros((ncurves, 2*(n+1)))
+    for i in range(ncurves):
+        depths[i], times[i] = get_zoft(tHol)
+        plt.plot(times[i], depths[i], lw=1)
+    plt.xlim((-0.1,tsim+0.1))
+    plt.ylim((-1,z0+1))
+    plt.gca().invert_xaxis()
+    plt.gca().invert_yaxis()
 
-#test = sim.fwd_profile(z, dzs, t, be10, h, lat)
+    plt.figure(2)
 
-#for i in range(ncurves):
+    z = np.linspace(0,20*100.0*rho,50)
+    ndzs = 0
+    conc = np.zeros((ncurves,len(z)))
+    for i in range(ncurves):
+        d = depths[i][1:-1]
+        ndzs = len(d) - 1
+        dz = np.zeros(ndzs)
+        for j in range(ndzs):
+            dz[j] = d[j+1] - d[j]
+        dz = dz.copy()[0::2]
+        t = 1e6 * times[i][1:]
+        conc[i] = sim.fwd_profile(z, dz, t, be10, h, lat)
+        plt.semilogx(conc[i], z / 100.0 / rho, lw=1)
+
+    plt.gca().invert_yaxis()
+    plt.show()
+
+    #test = sim.fwd_profile(z, dzs, t, be10, h, lat)
+
+    #for i in range(ncurves):
 
 
 
