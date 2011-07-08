db61d510815630cf81d6c68f273c9ccb8b5b50aa
Zach Ploskey <zploskey@uw.edu>
2011-07-08 12:21:39 -0700

    Changed software description.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='Cosmic',
       version='0.1',
-      description='Cosmic Ray Exposure Dating Utilities',
+      description='Cosmic Ray Exposure Utilities',
       author='Zach Ploskey',
       author_email='zploskey@uw.edu',
       packages=['cosmic'],
