ccfbafe7342eb69ddd67bf214639fb099da7413c
Zach Ploskey <zploskey@uw.edu>
2011-11-04 11:04:25 -0700

    Inversion result saving now platform independent

--- save_na_results.py
+++ save_na_results.py
@@ -2,6 +2,12 @@
 
 import os
 import sys
+import glob
+import shutil
+
+def copy_files(src_glob, dst_folder):
+    for fname in glob.iglob(src_glob):
+        shutil.copy(fname, os.path.join(dst_folder, fname))
 
 def save_na_results(folder_path=None):
     """Move the results of a Neighborhood Algorithm run to a folder with metadata.
@@ -14,23 +20,25 @@ def save_na_results(folder_path=None):
     but we should be careful not to introduce anything that will be copied, or
     at least modify this script accordingly if we do.
     """    
-    if sys.platform is 'win32':
-        raise NotImplementedError('Will not currently work on Windows.')     
     if folder_path is None:
         folder_path = sys.argv[1]
+    
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     else:
         raise Exception('Directory already exists; %s' % folder_path)
-    try:
-        os.system("mv *.npy %s" % folder_path)
-        os.system("mv *.txt %s" % folder_path)
-        os.system("mv *.png %s" % folder_path)
-        os.system("mv *.svg %s" % folder_path)
-        os.system("mv *.eps %s" % folder_path)
-        os.system("mv *.dat %s" % folder_path)
-    except OSError('Something went wrong while copying.'), e:
-        print 'Got:', e.args
+
+    problem = False
+    file_exts = ['npy', 'txt', 'png', 'svg', 'eps', 'dat']
+    for ext in file_exts:
+        try:
+            copy_files('*.' + ext, folder_path)
+        except Exception, e:
+            print 'Got:', e.args
+            problem = True
+    
+    if problem:
+        print 'An error was encountered while copying...'
     else:
         print 'Results were moved into %s' % folder_path
 
