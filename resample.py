3b7aa542598409349af9cf75acbe4fb925f18be1
Zach Ploskey <zploskey@uw.edu>
2011-11-04 11:58:56 -0700

    Platform agnostic filesaves.

--- resample.py
+++ resample.py
@@ -4,27 +4,34 @@ Created on Fri Oct 14 15:08:19 2011
 
 @author: zploskey
 """
+import os
+import sys
 
 import numpy as np
 import joblib
 
 import na
 
-# for debugging purposes
-#np.seterr(all='raise')
+argv = sys.argv
+data_dir = argv[1]
 
-data_dir = 'debug1/' # include trailing slash
+if not os.path.exists(data_dir):
+    raise Exception('Directory \'%s\' does not exist. Aborted.', data_dir)
 
-m = np.loadtxt(data_dir + 'models.txt')
-x2v = np.loadtxt(data_dir + 'errors.txt')
-con = joblib.load(data_dir + 'constraints.dat')
+if len(argv) > 2:
+    outfile = argv[2]
+else:
+    outfile = 'mr.txt'
+
+m = np.loadtxt(os.path.join(data_dir, 'models.txt'))
+x2v = np.loadtxt(os.path.join(data_dir, 'errors.txt'))
+con = joblib.load(os.path.join(data_dir, 'constraints.dat'))
 dof = con['dof']
-Nw = 100
-n = 1000
-
 d = m.shape[1]
-
 limits = (np.ones(d) * con['min_dz'], np.ones(d) * con['max_dz'])
 
+Nw = 2 # number of walks
+n = 2 # number of samples to take along each walk
+
 mr = na.resample(m, x2v, dof, Nw, n, limits, n_jobs=2)
-np.savetxt(data_dir + 'mr_100k.txt', mr)
+np.savetxt(os.path.join(data_dir, outfile), mr)
