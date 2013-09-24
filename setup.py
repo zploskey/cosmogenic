f2141d3b44513f4d722837c30310f8c8f81b1ee9
Zach Ploskey <zploskey@uw.edu>
2013-09-23 22:54:07 -0700

    Automatically build cython files
    Converted na.py to cython file. No annotations yet.
    Integrated cython into setup.py. The .pyx files are automatically
    cythonized as long as we aren't installing the source distribution.
    Generated C source files should be checked into source control.

--- setup.py
+++ setup.py
@@ -1,6 +1,31 @@
-#!/usr/bin/env python
+from __future__ import print_function
+
+import glob
+import multiprocessing
+import os
 
 from distutils.core import setup
+from distutils.extension import Extension
+
+def is_source_pkg():
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    return not os.path.exists(os.path.join(cwd, "PKG-INFO"))
+
+ext_names = ["na"]
+
+def get_exts():
+    if is_source_pkg():
+        exts = []
+        paths = glob.glob("cosmogenic/*.pyx")
+        for mod_path in paths:
+            mod = os.path.join("cosmogenic", 
+                mod_path.split(os.sep)[-1].split(".")[0])
+            exts.append(Extension(mod, [mod_path])) 
+    else:
+        from Cython.Build import cythonize
+        parallel_builds = 1.5 * multiprocessing.cpu_count()
+        exts = cythonize("cosmogenic/*.pyx", nthreads=parallel_builds)
+    return exts 
 
 setup(
     name = "cosmogenic",
@@ -26,4 +51,5 @@ setup(
         "numexpr (>=2.0)",
         "joblib (>=0.6)",
         ],
+    ext_modules = get_exts(),
 ) 
