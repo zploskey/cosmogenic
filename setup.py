0239cd38e4ff1811a48be92ac81cc52226b97475
Zach Ploskey <zploskey@gmail.com>
2014-02-14 16:35:01 -0800

    Remove cython dependency

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ import numpy
 import os
 
 from setuptools import setup
-from setuptools.extension import Extension
-from Cython.Build import cythonize
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 src_dir = os.path.join(current_dir, 'cosmogenic')
@@ -33,15 +31,4 @@ setup(
         "matplotlib (>=1.1)",
         "ipython (>=0.14)",
         ],
-    ext_modules=cythonize([
-        Extension(
-            "cosmogenic.na",
-            ["cosmogenic/na.pyx"],
-            libraries=["m"],
-            include_dirs=[numpy.get_include()]),
-        Extension(
-            "cosmogenic.cyrandom",
-            ["cosmogenic/cyrandom.pyx"],
-            include_dirs=[numpy.get_include(), src_dir]),
-        ])
     )
