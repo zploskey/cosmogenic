81aeecab52692bd3aac3a8adc13f68f784727813
Zach Ploskey <zploskey@gmail.com>
2014-07-02 13:53:26 -0700

    add parma test stub

---
+++ cosmogenic/tests/test_parma.py
@@ -0,0 +1,16 @@
+import unittest
+
+import numpy as np
+
+from cosmogenic import parma
+from TestBase import TestBase
+
+
+class TestParma(TestBase):
+
+    def setUp(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
