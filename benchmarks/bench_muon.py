c3171c7c5a6f711ada5b6b8d6f9fc8f5889a57e9
Zach Ploskey <zploskey@gmail.com>
2014-08-07 22:30:53 -0700

    remove unecessary imports in benchmark

--- benchmarks/bench_muon.py
+++ benchmarks/bench_muon.py
@@ -1,12 +1,6 @@
-from __future__ import print_function, division
 
-import numpy as np
-
-from cosmogenic import nuclide
-from cosmogenic import muon
+from cosmogenic import nuclide, muon
 
 n = nuclide.Be10Qtz()
 z = np.linspace(0, 3000, 15)
 p_mu = muon.P_mu_total(z=z, n=n)
-
-#print(p_mu)
