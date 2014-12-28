12d7942b2f91e4953055541c583399fd338e4fb9
Zach Ploskey <zach@ploskey.com>
2014-12-27 16:42:24 -0800

    Let N10 function do calculations for a blank (num atoms)

--- cosmogenic/datareduction.py
+++ cosmogenic/datareduction.py
@@ -8,14 +8,22 @@ N_A = const.N_A
 A_Be = 9.012182  # g / mol Be
 
 
-def N10(R10to9, Mq, Mc, n10b=0.0, unc_R10to9=0.0, unc_Mc=0.0, unc_n10b=0.0):
-
+def N10(R10to9, Mq=0.0, Mc=0.0, n10b=0.0, unc_R10to9=0.0, unc_Mc=0.0, unc_n10b=0.0):
+    
+    # If no quartz mass is provided,
+    # calculate a number of atoms instead of a concentration.
+    Mq = np.atleast_1d(Mq)
+    Mq[Mq == 0.0] = 1.0
+    Mq = np.array(Mq, dtype=np.float)
+    
     N10 = ((R10to9 * Mc * N_A / A_Be) - n10b) / Mq
 
-    # standard error propagation
-    # we assume error in quartz weighing is so small as to not matter
+    # Standard error propagation, ignoring error in quartz weighing.
     A = unc_R10to9 / Mq * (Mc * N_A / A_Be)
     B = unc_Mc / Mq * (R10to9 * N_A / A_Be)
+    # special case if unc_n10b is a pandas series
+    if hasattr(unc_n10b, 'values'):
+        unc_n10b = unc_n10b.values
     C = unc_n10b * -1 / Mq
     unc_N10 = np.sqrt(A ** 2 + B ** 2 + C ** 2)
 
